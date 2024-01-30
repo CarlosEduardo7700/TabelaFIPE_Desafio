1299ed00ce92bcfbcaa3490b791367950fbee4c3
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-01-30 17:27:17 -0300

    Criando a classe para consumir a API

--- src/main/java/br/com/alura/tabelafipe/controllers/MenuPrincipal.java
+++ src/main/java/br/com/alura/tabelafipe/controllers/MenuPrincipal.java
@@ -1,7 +1,19 @@
 package br.com.alura.tabelafipe.controllers;
 
+import br.com.alura.tabelafipe.service.ConsumirAPI;
+
+import java.util.Scanner;
+
 public class MenuPrincipal {
+
+    private ConsumirAPI consumirAPI = new ConsumirAPI();
+
+    private Scanner scan = new Scanner(System.in);
+
     public void exibir() {
-        System.out.println("Iniciando aplicação...");
+        System.out.println("\nInforme o tipo de veículo que você procura ('carros', 'motos', 'caminhoes'):");
+        String veiculo = scan.nextLine();
+        var json = consumirAPI.obterDados("https://parallelum.com.br/fipe/api/v1/" + veiculo + "/marcas");
+        System.out.println(json);
     }
 }
